b0234d9ebe7e0ab633cdab2a8c028673c3354ac6
Juan Pablo Comparada <110425949+ComparadaChucho@users.noreply.github.com>
2024-11-27 09:57:46 -0300

    Episode 6

--- Learning PHP/index.php
+++ Learning PHP/index.php
@@ -1,35 +1,32 @@
-<!doctype html>
+<<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    <style>
-        body{
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
 <body>
-    <?php
-        $name = "Dark Matter";
-        $read = True;
+    <h1>Recommended Books</h1>
 
-        if ($read){
-            $message = "You have read $name";
-        } else {
-            $message = "You have not read $name";
-        }
+    <?php
+        $books = [
+                "Do Androids Dream of Electric Sheep",
+                "The Langoliers",
+                "Hail Mary"
+        ];
     ?>
 
+    <ul>
+        <?php foreach ($books as $book){
+            echo "<li>$book</li>";
+        }
+        ?>
+    </ul>
 
-    <h1>
-        <?= $message ?>
-        <!-- <?php echo $message; ?> = <?= $message ?> -->
-    </h1>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book ?>></li>
+        <?php endforeach; ?>
+    </ul>
 
 </body>
 </html>
