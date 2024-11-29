6c0006a036913ba2b1f6283ed5835e24906f753f
Juan Pablo Comparada <110425949+ComparadaChucho@users.noreply.github.com>
2024-11-29 09:25:07 -0300

    Episode 15 update
    Fixed issues

--- Learning PHP/views/partials/nav.php
+++ Learning PHP/views/partials/nav.php
@@ -11,10 +11,10 @@
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                                 <a href="/" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>"
                                    aria-current="page">Home</a>
-                                <a href="/about.php"
-                                   class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">About</a>
-                                <a href="/contact.php"
-                                   class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Contact</a>
+                                <a href="/about"
+                                   class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/controllers/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">About</a>
+                                <a href="/contact"
+                                   class="rounded-md px-3 py-2 text-sm font-medium <?= urlIs('/controllers/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Contact</a>
                             </div>
                         </div>
                     </div>
