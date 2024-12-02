58b2ecab516dedfb7a470db07dfe8a2812a5df80
Juan Pablo Comparada <110425949+ComparadaChucho@users.noreply.github.com>
2024-12-02 09:44:16 -0300

    Doing Episode 17

--- Learning PHP/index.php
+++ Learning PHP/index.php
@@ -1,4 +1,24 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+class Person {
+    public $name;
+    public $age;
+
+    public function breathe()
+    {
+        echo $this-> name . ' is breathing';
+    }
+}
+
+$person = new Person();
+
+$person -> name = 'John Doe';
+$person -> age = 25;
+
+//dd($person);
+//dd($person->name);
+//dd($person->age);
+$person->breathe();
