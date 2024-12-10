70b168a6d5d8bd49d8f9bdeac2a5c68c202fd256
Juan Pablo Comparada <110425949+ComparadaChucho@users.noreply.github.com>
2024-12-10 10:33:03 -0300

    Episode 31

--- Learning PHP/Core/Database.php
+++ Learning PHP/Core/Database.php
@@ -1,5 +1,9 @@
 <?php
 
+Namespace Core;
+
+use PDO;
+
 class Database
 {
     public $connection;
--- Learning PHP/Core/Response.php
+++ Learning PHP/Core/Response.php
@@ -1,5 +1,7 @@
 <?php
 
+Namespace Core;
+
 class Response {
     const NOT_FOUND = 404;
     const FORBIDDEN = 403;
