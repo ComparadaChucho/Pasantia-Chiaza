95ef223eb4e908cd0b47dc3d88f1c2dc51b4225a
Juan Pablo Comparada <110425949+ComparadaChucho@users.noreply.github.com>
2025-02-28 13:27:29 -0300

    29

--- Learning Laravel/pixel-positions/resources/views/components/job-card-wide.blade.php
+++ Learning Laravel/pixel-positions/resources/views/components/job-card-wide.blade.php
@@ -1,28 +1,29 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">    
     <div class="mt-auto mb-auto">
         <x-employer-logo />
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a class="self-start text-sm text-gray-500">Laracast</a>
+        <a class="self-start text-sm text-gray-500">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{ $job->title }}</h3>
 
         <div class="mt-8">
-            <p class="text-sm text-gray-500 mt-auto">Full Time = From $60,000</p>
+            <p class="text-sm text-gray-500 mt-auto">{{ $job->salary }}</p>
         </div>
     </div>
 
     <div class="flex flex-col items-end">
         <div>
-            <x-tag-transparent size="small">Remote</x-tag-transparent>
-            <x-tag-transparent size="small">22h</x-tag-transparent>
+            <x-tag-transparent :tag="$job->schedule" size="small" />
         </div>
 
         <div class="mt-auto">
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">API</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :tag="$tag" size="small" />
+            @endforeach
         </div>
     </div>
 </x-panel>
--- Learning Laravel/pixel-positions/resources/views/components/job-card.blade.php
+++ Learning Laravel/pixel-positions/resources/views/components/job-card.blade.php
@@ -1,18 +1,20 @@
+@props(['job'])
+
 <x-panel class="flex flex-col">
-    <div class="self-start text-sm">Laracast</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-7 text-center">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm mt-4">Full Time = From $60,000</p>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between mt-auto">
         <div class="mt-auto">
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">API</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :tag="$tag" size="small" />
+            @endforeach
         </div>
 
-        <x-employer-logo width="42" />
+        <x-employer-logo :width="42" />
     </div>
 </x-panel>
