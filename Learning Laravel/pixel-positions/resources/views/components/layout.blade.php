ee3460e82ca2a1257d8efc2698d9fd85d320686e
Juan Pablo Comparada <110425949+ComparadaChucho@users.noreply.github.com>
2025-03-14 13:07:59 -0300

    30

--- Learning Laravel/pixel-positions/resources/views/components/layout.blade.php
+++ Learning Laravel/pixel-positions/resources/views/components/layout.blade.php
@@ -13,7 +13,7 @@
         rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-custom-black text-white font-hanken">
+<body class="bg-custom-black text-white font-hanken pb-20">
     
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
@@ -30,9 +30,25 @@
                 <a href="">Companies</a>
             </div>
 
-            <div>
-                <a href="">Post a Job</a>
-            </div>
+            @auth
+                <div class="space-x-6 font-bold flex">
+                    <a href="/jobs/create">Post a Job</a>
+
+                    <form action="POST" action="/logout">
+                        @csrf
+                        @method('DELETE')
+
+                        <button>Log Out</button>
+                    </form>
+                </div>
+            @endauth
+
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Log In</a>
+                </div>
+            @endguest
         </nav>
 
         <main class="mt-10 max-w-[986px] mx-auto">
